c6f195e6c83c4fe30484f8ceca9432f0b8314aac
CarvedCoder <phionex2233@gmail.com>
2025-04-13 20:26:49 +0530

    loop add

---
+++ Python_Learn/loops.py
@@ -0,0 +1,12 @@
+name = "Shreyans"
+# for loop in python
+
+for i in range (len(name)):
+    print(name[i])
+
+
+# while loop in python
+j = 0
+while j < len(name):
+    print(name[j])
+    j = j + 1
